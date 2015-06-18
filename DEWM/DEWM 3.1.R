69c4c9f54622397c93bd18a4a31ac0e92f5a3903
MichaelGarrison89 <garrim@DX113163.nu.com>
2015-06-18 15:08:47 -0400

    DEWM

--- DEWM/DEWM 3.1.R
+++ DEWM/DEWM 3.1.R
@@ -45,28 +45,28 @@ NURev <- read.csv("NU Demand Revenues.csv",
 
 nstar <- read.csv("NSTAR-HourlyLoads.csv", 
                   header=TRUE, stringsAsFactors=FALSE) %>%
-  mutate(date = as.Date(Date,"%m/%d/%Y")) %>%
-  mutate(day = as.numeric(getDay(date))) %>%
-  mutate(month = as.numeric(getMonth(date))) %>%
-  mutate(year = as.numeric(getYear(date)))
+         mutate(date = as.Date(Date,"%m/%d/%Y")) %>%
+         mutate(day = as.numeric(getDay(date))) %>%
+         mutate(month = as.numeric(getMonth(date))) %>%
+         mutate(year = as.numeric(getYear(date)))
 
 beco <- select(nstar, date, day, month, year, contains("Beco")) %>%
-  gather("variable", "load", contains("Beco")) %>%
-  mutate(hour = as.numeric(str_sub(variable, 9L)))
+        gather("variable", "load", contains("Beco")) %>%
+        mutate(hour = as.numeric(str_sub(variable, 9L)))
 
 
 
 NSTARRev <- read.csv("NSTAR Demand Revenues.csv", 
                      header=TRUE, stringsAsFactors=FALSE) %>%
-  filter(Year >= 2011)
+            filter(Year >= 2011)
 
 clp <- read.csv("CL&P Daily Peaks.csv", 
                 header=TRUE, stringsAsFactors=FALSE) %>%
-  mutate(date = as.Date(Date,"%m/%d/%Y")) %>%
-  mutate(day = as.numeric(getDay(date))) %>%
-  mutate(month = as.numeric(getMonth(date))) %>%
-  mutate(year = as.numeric(getYear(date))) %>%
-  rename(clp = CL.P_Daily)
+       mutate(date = as.Date(Date,"%m/%d/%Y")) %>%
+       mutate(day = as.numeric(getDay(date))) %>%
+       mutate(month = as.numeric(getMonth(date))) %>%
+       mutate(year = as.numeric(getYear(date))) %>%
+       rename(clp = CL.P_Daily)
 
 # This file was created by a separate program that optimizes the DEWM peaks.
 
@@ -78,18 +78,18 @@ exponent <- read.csv("Exponent.csv", header=TRUE, stringsAsFactors=FALSE)
 
 beco.means <-  filter(beco, month == est.month, 
                       day > est.day, year < est.year) %>%
-  group_by(year) %>%
-  mutate(rank = row_number(load))%>%
-  group_by(rank, add = FALSE) %>%
-  summarise(load = mean(load))
+               group_by(year) %>%
+               mutate(rank = row_number(load))%>%
+               group_by(rank, add = FALSE) %>%
+               summarise(load = mean(load))
 
 
 clp.means <-  filter(clp, month == est.month, 
                      day > est.day, year < est.year) %>%
-  group_by(year) %>%
-  mutate(rank = row_number(clp)) %>%
-  group_by(rank, add = FALSE) %>%
-  summarise(load = mean(clp))
+              group_by(year) %>%
+              mutate(rank = row_number(clp)) %>%
+              group_by(rank, add = FALSE) %>%
+              summarise(load = mean(clp))
 
 
 # Now we will calculate our historical DEWM peaks, which are monthly values. 
@@ -97,33 +97,34 @@ clp.means <-  filter(clp, month == est.month,
 # DEWM peaks and revenues.
 
 beco.history <- filter(beco, date <= est.date - est.day) %>%
-  mutate(exp = load ^ exponent$Beco) %>%
-  group_by(year, month) %>%
-  mutate(weight = exp / sum(exp)) %>%
-  mutate(dewm = weight * load) %>%
-  summarise(dewm = sum(dewm), check = sum(weight)) %>%
-  left_join(select(NSTARRev, -Comm, -Camb), 
-            by = c("month" = "Month", "year" = "Year")) %>%
-  mutate(alpha = BECo / dewm)
+                mutate(exp = load ^ exponent$Beco) %>%
+                group_by(year, month) %>%
+                mutate(weight = exp / sum(exp)) %>%
+                mutate(dewm = weight * load) %>%
+                summarise(dewm = sum(dewm), check = sum(weight)) %>%
+                left_join(select(NSTARRev, -Comm, -Camb), 
+                          by = c("month" = "Month", "year" = "Year")) %>%
+                mutate(alpha = BECo / dewm)
 
 
 # Because of the ratcheted rates, CL&P behaves a little differently.
 
 clp.history <- filter(clp, date <= est.date - est.day) %>%
-  mutate(exp = clp ^ exponent$CLP) %>%
-  mutate(load.exp = clp * exp) %>%
-  mutate(c.exp = rollsum(exp, 365, 
+               mutate(exp = clp ^ exponent$CLP) %>%
+               mutate(load.exp = clp * exp) %>%
+               mutate(c.exp = rollsum(exp, 365, 
                          fill = NA, align = "right")) %>%
-  mutate(c.load.exp = rollsum(load.exp, 365,
-                              fill = NA, align = "right")) %>%
-  mutate(dewm = c.load.exp / c.exp) %>%
-  mutate(last.day.of.month = 
-           ifelse(lead(month, 1) != month, 1, 0)) %>%
-  filter(last.day.of.month == 1 | is.na(last.day.of.month), year > 2009) %>%
-  select(year, month, dewm) %>%
-  left_join(select(NURev, -WMECORev, -PSNHRev),
-            by = c("month" = "Month", "year" = "Year")) %>%
-  mutate(alpha = CLPRev / dewm)
+               mutate(c.load.exp = rollsum(load.exp, 365,
+                                           fill = NA, align = "right")) %>%
+               mutate(dewm = c.load.exp / c.exp) %>%
+               mutate(last.day.of.month = 
+                      ifelse(lead(month, 1) != month, 1, 0)) %>%
+               filter(last.day.of.month == 1 | is.na(last.day.of.month), 
+                      year > 2009) %>%
+               select(year, month, dewm) %>%
+               left_join(select(NURev, -WMECORev, -PSNHRev),
+                         by = c("month" = "Month", "year" = "Year")) %>%
+               mutate(alpha = CLPRev / dewm)
 
 
 # And now we'll combine the data we have for the month with the historical 
@@ -131,27 +132,27 @@ clp.history <- filter(clp, date <= est.date - est.day) %>%
 
 
 beco.current.dewm <- filter(beco, month == est.month, year == est.year) %>%
-  select(load) %>%
-  rbind(select(beco.means, load)) %>%
-  mutate(exp = load ^ exponent$Beco) %>%
-  mutate(weight = exp / sum(exp)) %>%
-  mutate(dewm = weight * load) %>%
-  summarise(dewm = sum(dewm))
+                     select(load) %>%
+                     rbind(select(beco.means, load)) %>%
+                     mutate(exp = load ^ exponent$Beco) %>%
+                     mutate(weight = exp / sum(exp)) %>%
+                     mutate(dewm = weight * load) %>%
+                     summarise(dewm = sum(dewm))
 
 
 # Again, CLP takes a different form because of the ratcheted rates.
 
 clp.dewm <- select(clp, clp) %>%
-  rename(load = clp) %>%
-  rbind(select(clp.means, load)) %>%
-  mutate(exp = load ^ exponent$CLP) %>%
-  mutate(load.exp = load * exp) %>%
-  mutate(c.exp = rollsum(exp, 365, 
-                         fill = NA, align = "right")) %>%
-  mutate(c.load.exp = rollsum(load.exp, 365,
-                              fill = NA, align = "right")) %>%
-  mutate(dewm = c.load.exp / c.exp) %>%
-  summarise(dewm = last(dewm))
+            rename(load = clp) %>%
+            rbind(select(clp.means, load)) %>%
+            mutate(exp = load ^ exponent$CLP) %>%
+            mutate(load.exp = load * exp) %>%
+            mutate(c.exp = rollsum(exp, 365, 
+                                   fill = NA, align = "right")) %>%
+            mutate(c.load.exp = rollsum(load.exp, 365,
+                                        fill = NA, align = "right")) %>%
+            mutate(dewm = c.load.exp / c.exp) %>%
+            summarise(dewm = last(dewm))
 
 
 # Finally we'll calculate revenue numbers for the current month for each OpCo,
@@ -161,22 +162,22 @@ clp.dewm <- select(clp, clp) %>%
 
 beco.revenue.estimate <- filter(beco.history, month == est.month,
                                 year >= est.year - 2, year < est.year) %>%
-  group_by(month) %>%
-  summarise(alpha = mean(alpha)) %>%
-  mutate(beco = alpha * beco.current.dewm) %>%
-  select(-alpha)
+                         group_by(month) %>%
+                         summarise(alpha = mean(alpha)) %>%
+                         mutate(beco = alpha * beco.current.dewm) %>%
+                         select(-alpha)
 
 
 clp.revenue.estimate <- filter(clp.history, month == est.month,
                                year >= est.year - 2, year < est.year) %>%
-  group_by(month) %>%
-  summarise(alpha = mean(alpha)) %>%
-  mutate(clp = alpha * clp.dewm) %>%
-  select(-alpha)
+                        group_by(month) %>%
+                        summarise(alpha = mean(alpha)) %>%
+                        mutate(clp = alpha * clp.dewm) %>%
+                        select(-alpha)
 
 # And now we prepare our table for export.
 
 export.table <- data.frame(month = est.month,
                            year  = est.year) %>%
-  mutate(beco = as.numeric(beco.revenue.estimate$beco)) %>%
-  mutate(clp = as.numeric(clp.revenue.estimate$clp))
+                mutate(beco = as.numeric(beco.revenue.estimate$beco)) %>%
+                mutate(clp = as.numeric(clp.revenue.estimate$clp))
